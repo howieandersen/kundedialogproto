cf3e725771c69c79871016b28348bc39d08df800
Håvard Andersen <havard.andersen@innovasjonnorge.no>
2019-05-23 00:28:25 +0200

    Updates

---
+++ precache-manifest.102b97874728cd4e8dc63c1bb077723b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05131d4eed9114947707d38fd1bfab7b",
+    "url": "/kundedialogproto/index.html"
+  },
+  {
+    "revision": "02a8b36e67ca236780f3",
+    "url": "/kundedialogproto/static/css/main.b0948fa4.chunk.css"
+  },
+  {
+    "revision": "141469e4bb6059c2bfd3",
+    "url": "/kundedialogproto/static/js/2.16effa7c.chunk.js"
+  },
+  {
+    "revision": "02a8b36e67ca236780f3",
+    "url": "/kundedialogproto/static/js/main.4a7967eb.chunk.js"
+  },
+  {
+    "revision": "a2a4c14b839e30e847f1",
+    "url": "/kundedialogproto/static/js/runtime~main.3bc7d88d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/howieandersen/kundedialogproto/precache-manifest.e57311aecabebe96decec3355792aa3d.js"
+  "/kundedialogproto/precache-manifest.102b97874728cd4e8dc63c1bb077723b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/howieandersen/kundedialogproto/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kundedialogproto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
