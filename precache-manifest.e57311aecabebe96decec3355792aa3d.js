a4bc9d47f99095ff7ca01f9862202bcf2fc76f2c
Håvard Andersen <havard.andersen@innovasjonnorge.no>
2019-05-23 00:17:10 +0200

    Updates

---
+++ precache-manifest.e57311aecabebe96decec3355792aa3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f3bd198cf05f4ebd5791658e79a0f2",
+    "url": "/howieandersen/kundedialogproto/index.html"
+  },
+  {
+    "revision": "3c6b02fca9d9e9671366",
+    "url": "/howieandersen/kundedialogproto/static/css/main.b0948fa4.chunk.css"
+  },
+  {
+    "revision": "141469e4bb6059c2bfd3",
+    "url": "/howieandersen/kundedialogproto/static/js/2.16effa7c.chunk.js"
+  },
+  {
+    "revision": "3c6b02fca9d9e9671366",
+    "url": "/howieandersen/kundedialogproto/static/js/main.870f347f.chunk.js"
+  },
+  {
+    "revision": "b4989f224349bdb0e5b0",
+    "url": "/howieandersen/kundedialogproto/static/js/runtime~main.5580ef81.js"
+  }
+]);
